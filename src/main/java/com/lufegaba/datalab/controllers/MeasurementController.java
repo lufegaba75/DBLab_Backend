f7648f1cb038645c67329163e8af511b66a01bde
lufegaba75 <lufegaba75@gmail.com>
2024-06-16 18:16:41 +0200

    fix: basic methods implemented on Measurement Service & Controller

--- src/main/java/com/lufegaba/datalab/controllers/MeasurementController.java
+++ src/main/java/com/lufegaba/datalab/controllers/MeasurementController.java
@@ -1,10 +1,14 @@
 package com.lufegaba.datalab.controllers;
 
+import com.lufegaba.datalab.model.entities.analysis.Measurement;
 import com.lufegaba.datalab.services.MeasurementService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -13,4 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class MeasurementController {
 
     private final MeasurementService measurementService;
+
+    @PostMapping
+    public ResponseEntity<Measurement> createMeasurement (@RequestBody @Valid Measurement measurement) {
+        return ResponseEntity.ok(measurementService.createNewMeasurement(measurement));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Measurement>> getAllMeasurements () {
+        return ResponseEntity.ok(measurementService.findAllMeasurements());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Measurement> findMeasurementById (@PathVariable Long id) {
+        return ResponseEntity.ok(measurementService.findMeasurementById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteMeasurementById (@PathVariable Long id) {
+        measurementService.deleteMeasurementById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/lufegaba/datalab/services/MeasurementService.java
+++ src/main/java/com/lufegaba/datalab/services/MeasurementService.java
@@ -1,9 +1,12 @@
 package com.lufegaba.datalab.services;
 
+import com.lufegaba.datalab.model.entities.analysis.Measurement;
 import com.lufegaba.datalab.model.repositories.MeasurementRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -12,5 +15,20 @@ public class MeasurementService {
 
     private final MeasurementRepository measurementRepository;
 
+    public List<Measurement> findAllMeasurements() {
+        return measurementRepository.findAll();
+    }
+
+    public Measurement findMeasurementById (Long id) {
+        return measurementRepository.findById(id).orElseThrow();
+    }
+
+    public Measurement createNewMeasurement (Measurement measurement) {
+        return measurementRepository.save(measurement);
+    }
+
+    public void deleteMeasurementById (Long id) {
+        measurementRepository.deleteById(id);
+    }
 
 }
